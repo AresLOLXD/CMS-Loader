88961822c5e48a57fcfa9b664c8f278fe9998926
Ares Ulises Juárez Martínez <aresulises8@hotmail.com>
2024-10-08 01:34:47 -0600

    a

--- src/controllers/views.ts
+++ src/controllers/views.ts
@@ -4,6 +4,7 @@ const router = Router()
 router.get("/seleccionaColumnasUser", (req: Request, res: Response) => {
     const { registros, columnas } = req.session
     if (!registros || !columnas) {
+        console.log("Session :", req.session)
         res.redirect(`cargaUsuarios.html`)
     } else {
         res.render("seleccionaColumnasUser.ejs", {
@@ -16,6 +17,7 @@ router.get("/seleccionaColumnasUser", (req: Request, res: Response) => {
 router.get("/seleccionaColumnasConcurso", (req: Request, res: Response) => {
     const { registros, columnas } = req.session
     if (!registros || !columnas) {
+        console.log("Session :", req.session)
         res.redirect(`cargaConcurso.html`)
     } else {
         res.render("seleccionaColumnasConcurso.ejs", {
