7e7704ef9390ef7c4b1f2c5287eafeaa635eb16e
Ares Ulises Juárez Martínez <aresulises8@hotmail.com>
2024-10-09 15:02:14 -0600

    Unas ultimas pruebas

--- src/index.ts
+++ src/index.ts
@@ -34,6 +34,8 @@ app.use(session({
     },
     resave: true,
     saveUninitialized: true,
+    name: "CMS_Loader",
+    proxy: true,
 }))
 app.use((req, _res, next) => {
     req.session.saveAsync = promisify(req.session.save.bind(req.session));
