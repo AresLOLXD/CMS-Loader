772d139a1fe221f6a343ba3ab5c3cfd751822611
Ares Ulises Juárez Martínez <aresulises8@hotmail.com>
2024-10-09 16:17:49 -0600

    a

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ app.use(session({
     cookie: {
         secure: true,
         maxAge: 1000 * 60 * 5,
-        sameSite: false,
+        sameSite: "none",
     },
     resave: true,
     saveUninitialized: true,
